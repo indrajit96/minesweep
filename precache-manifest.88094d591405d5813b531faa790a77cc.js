f62472bfebd16acff761604bece1a30c215cebf4
Indrajit Maloji Bhosale <ibhosale@adobe.com>
2020-07-05 10:23:32 +0530

    Updates

---
+++ precache-manifest.88094d591405d5813b531faa790a77cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac205ac0d6721f9a72bf47d3b47c8868",
+    "url": "/minesweep/index.html"
+  },
+  {
+    "revision": "09c1c102f000faf1a178",
+    "url": "/minesweep/static/css/main.584f663b.chunk.css"
+  },
+  {
+    "revision": "0bccfd9b2ec3e016d67d",
+    "url": "/minesweep/static/js/2.22cae8a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/minesweep/static/js/2.22cae8a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09c1c102f000faf1a178",
+    "url": "/minesweep/static/js/main.e69a8f8c.chunk.js"
+  },
+  {
+    "revision": "24f6af1ee0ad7db5cee6",
+    "url": "/minesweep/static/js/runtime-main.5387a656.js"
+  }
+]);
